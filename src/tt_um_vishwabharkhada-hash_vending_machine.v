97eff59874a7cc4d9821efb6edb83e96a5f36ab7
vishwabharkhada-hash <vishwabharkhada@gmail.com>
2026-06-22 10:50:16 +0200

    Update and rename project.v to tt_um_vishwabharkhada-hash_vending_machine.v

---
+++ src/tt_um_vishwabharkhada-hash_vending_machine.v
@@ -0,0 +1,53 @@
+/*
+ * Copyright (c) 2024 Your Name
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+`default_nettype none
+
+module tt_um_example (
+    input  wire [7:0] ui_in,    // Dedicated inputs
+    output wire [7:0] uo_out,   // Dedicated outputs
+    input  wire [7:0] uio_in,   // IOs: Input path
+    output wire [7:0] uio_out,  // IOs: Output path
+    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
+    input  wire       ena,      // always 1 when the design is powered, so you can ignore it
+    input  wire       clk,      // clock
+    input  wire       rst_n     // reset_n - low to reset
+);
+
+    wire onlinepayment_selected = ui_in[0];
+    wire [3:0] selected_product_number = ui_in[4:1];
+    wire online_payment_success = ui_in[5];
+
+    wire show_product_number;
+    wire show_payment_status;
+    wire show_output_status;
+
+    vending_machine_fsm dut(
+        .clk(clk),
+        .reset_n(rst_n),
+        .onlinepayment_selected(onlinepayment_selected),
+        .selected_product_number(selected_product_number),
+        .online_payment_success(online_payment_success),
+        .show_product_number(show_product_number),
+        .show_payment_status(show_payment_status),
+        .show_output_status(show_output_status)
+    );
+
+    
+
+  // All output pins must be assigned. If not used, assign to 0.
+    assign uo_out[0] = show_product_number;
+    assign uo_out[1] = show_payment_status;
+    assign uo_out[2] = show_output_status;
+    assign uo_out[7:3] = 0;
+
+    assign uio_out = 0;
+    assign uio_oe = 0;
+
+
+  // List all unused inputs to prevent warnings
+    wire _unused = &{ena, uio_in, 1'b0};
+
+endmodule
